e1c95a4339ebbfd6a4917c3e1c7666b8f6cb51f6
Tiago Silva <tiagosilva@multicast.com.br>
2022-11-04 19:22:38 -0300

    Authentication

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const userRoute = require("./routes/user")
+const authRoute = require("./routes/auth")
 
 dotenv.config();
 
@@ -13,7 +14,9 @@ mongoose.connect(process.env.MONGO_URL)
   });
 
 app.use(express.json());
+app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
+
 
 app.listen(process.env.PORT || 5000, () => {
   console.log("Running")
