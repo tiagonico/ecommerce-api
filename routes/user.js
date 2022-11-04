85fb9b86139df095377b9c13652fe54bdcfd35b5
Tiago Silva <tiagosilva@multicast.com.br>
2022-11-04 18:26:09 -0300

    Creating Models

--- routes/user.js
+++ routes/user.js
@@ -1,12 +1,4 @@
 const router = require("express").Router();
 
-router.get("/usertest", (req,res) => {
-  res.send("user test ok");
-})
-
-router.post("/userposttest", (req,res) => {
-  const username = req.body.username
-  res.send(username);
-})
 
 module.exports = router
