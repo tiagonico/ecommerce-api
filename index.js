886fca7e257e827c85d33757bc61633894bcceb2
Tiago Silva <tiagosilva@multicast.com.br>
2022-11-06 12:50:11 -0300

    Adding CORS

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const cors = require('cors')
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const userRoute = require("./routes/user")
@@ -16,6 +17,7 @@ mongoose.connect(process.env.MONGO_URL)
     console.log(err)
   });
 
+app.use(cors())
 app.use(express.json());
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
